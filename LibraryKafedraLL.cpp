81840577ea8296724f79858357f2e8068e45883f
Артем Мишко <Artem2281935@outlook.com>
2025-05-13 20:21:34 +0300

    update

---
+++ LibraryKafedraLL.cpp
@@ -0,0 +1,20 @@
+﻿#define NOMINMAX     // Виправлення проблеми з неоднозначністю byte
+#define WIN32_LEAN_AND_MEAN  // Зменшення конфліктів заголовних файлів
+
+#include "LibraryKafedraLL.h"
+
+using namespace System;
+using namespace System::Windows::Forms;
+
+// Точка входу в програму
+[STAThread]
+int main() {
+    setlocale(LC_ALL, "Ukrainian");
+
+    // Запуск Windows Forms додатку
+    Application::EnableVisualStyles();
+    Application::SetCompatibleTextRenderingDefault(false);
+    Application::Run(gcnew Library::LibraryForm());
+
+    return 0;
+}
